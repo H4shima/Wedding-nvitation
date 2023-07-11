e4760ef32abcc24dbc08f988d51baccb4e53f92a
Hashima <106578638+H4shima@users.noreply.github.com>
2023-07-11 22:46:17 +0300

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let scrollToTop = function () {
+  let scrollProgress = $(document).scrollTop();
+  
+  if (scrollProgress > 100) {
+    $('.scroll-to-top-button').css("opacity", "1");
+  } else {
+    $('.scroll-to-top-button').css("opacity", "0");    
+  }
+} 
+
+$('.scroll-to-top-button').click(function () {
+  $('html,body').animate({
+    scrollTop: 0
+  }, 500);
+});
+
+scrollToTop();
+$(document).scroll(scrollToTop);
